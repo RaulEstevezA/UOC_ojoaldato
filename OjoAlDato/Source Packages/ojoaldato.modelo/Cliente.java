787ce29b240da434eefa1c2400258a377045dde0
RaulEstevezA <tevezabella@hotmail.com>
2025-10-07 01:59:14 +0200

    "Creada la configuracion del proyecto y añadido el archivo Clase Cliente"

---
+++ OjoAlDato/Source Packages/ojoaldato.modelo/Cliente.java
@@ -0,0 +1,9 @@
+package ojoaldato.modelo;
+
+/**
+ * Clase abstracta base del modelo que representa a un cliente.
+ * Se completará con atributos y métodos según el UML definitivo (AA1).
+ */
+public abstract class Cliente {
+    // TODO: definir atributos y métodos concretos cuando el diagrama esté cerrado
+}
