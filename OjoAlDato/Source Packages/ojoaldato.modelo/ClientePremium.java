aa1026c967a1b99278f2d297d68a9ab204daf268
Andrea Ros <androsrivas@outlook.com>
2025-10-09 15:15:06 +0200

    crear subclases ClienteEstandar y ClientePremium con sus getters, setters, toStringg y étodos heredados

--- OjoAlDato/Source Packages/ojoaldato.modelo/ClientePremium.java
+++ OjoAlDato/Source Packages/ojoaldato.modelo/ClientePremium.java
@@ -4,5 +4,24 @@ package ojoaldato.modelo;
  * Subclase de Cliente que representa un cliente premium.
  */
 public class ClientePremium extends Cliente {
-    // TODO: atributos y métodos específicos de ClientePremium
+    private static BigDecimal descuentoEnvio = 0.8;
+
+    public ClientePremium() {}
+    public ClientePremium(String nombre, String domicilio, String nif, String email, BigDecimal cuota, BigDecimal descuentoEnvio) {
+        super(nombre, domicilio, nif, email, cuota);
+        this.descuentoEnvio = descuentoEnvio;
+    }
+
+    @Override
+    public BigDecimal calcularGastosEnvio(BigDecimal base) {
+        return base.multiply(descuentoEnvio);
+    }
+
+    public static BigDecimal getDescuentoEnvio() {
+        return descuentoEnvio;
+    }
+
+    public static void setDescuentoEnvio(BigDecimal descuentoEnvio) {
+        ClientePremium.descuentoEnvio = descuentoEnvio;
+    }
 }
