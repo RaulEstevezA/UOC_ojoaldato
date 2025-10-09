4d6a0dc29143dcd3e2834cb720411317edb5a265
Andrea Ros <androsrivas@outlook.com>
2025-10-09 15:07:12 +0200

    crear clase abstracta Cliente con sus atributos, getters, setters, toString y método abstracto

--- OjoAlDato/Source Packages/ojoaldato.modelo/Cliente.java
+++ OjoAlDato/Source Packages/ojoaldato.modelo/Cliente.java
@@ -5,5 +5,70 @@ package ojoaldato.modelo;
  * Se completará con atributos y métodos según el UML definitivo (AA1).
  */
 public abstract class Cliente {
-    // TODO: definir atributos y métodos concretos cuando el diagrama esté cerrado
+    private String nombre;
+    private String domicilio;
+    private String nif;
+    private String email;
+    private BigDecimal cuota;
+
+    public Cliente() {}
+    public Cliente(String nombre, String domicilio, String nif, String email, BigDecimal cuota) {
+        this.nombre = nombre;
+        this.domicilio = domicilio;
+        this.nif = nif;
+        this.email = email;
+        this.cuota = cuota;
+    }
+
+    public abstract BigDecimal calcularGastosEnvio(BigDecimal base);
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    public String getNif() {
+        return nif;
+    }
+
+    public void setNif(String nif) {
+        this.nif = nif;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public BigDecimal getCuota() {
+        return cuota;
+    }
+
+    public void setCuota(BigDecimal cuota) {
+        this.cuota = cuota;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente: \n" +
+                "Nombre = " + nombre + '\n' +
+                "Domicilio = " + domicilio + '\n' +
+                "NIF = " + nif + '\n' +
+                "Email = " + email + '\n' +
+                "Cuota = " + cuota + '\n';
+    }
 }
