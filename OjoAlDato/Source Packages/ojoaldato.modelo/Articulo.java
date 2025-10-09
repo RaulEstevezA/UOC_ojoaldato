feaf97107570e41069bc3967c40d4c637d0656a6
Andrea Ros <androsrivas@outlook.com>
2025-10-09 15:18:48 +0200

    crear clase Artículo con sus getters, setters y toString

--- OjoAlDato/Source Packages/ojoaldato.modelo/Articulo.java
+++ OjoAlDato/Source Packages/ojoaldato.modelo/Articulo.java
@@ -4,5 +4,68 @@ package ojoaldato.modelo;
  * Clase que representa un artículo disponible en el sistema.
  */
 public class Articulo {
-    // TODO: atributos y métodos según el UML definitivo
+    private String codigo;
+    private String descripcion;
+    private BigDecimal pvp;
+    private BigDecimal gastosEnvio;
+    private int tiempoPreparacion;
+
+    public Articulo() {}
+    public Articulo(String codigo, String descripcion, BigDecimal pvp, BigDecimal gastosEnvio, int tiempoPreparacion) {
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+        this.pvp = pvp;
+        this.gastosEnvio = gastosEnvio;
+        this.tiempoPreparacion = tiempoPreparacion;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public BigDecimal getPvp() {
+        return pvp;
+    }
+
+    public void setPvp(BigDecimal pvp) {
+        this.pvp = pvp;
+    }
+
+    public BigDecimal getGastosEnvio() {
+        return gastosEnvio;
+    }
+
+    public void setGastosEnvio(BigDecimal gastosEnvio) {
+        this.gastosEnvio = gastosEnvio;
+    }
+
+    public int getTiempoPreparacion() {
+        return tiempoPreparacion;
+    }
+
+    public void setTiempoPreparacion(int tiempoPreparacion) {
+        this.tiempoPreparacion = tiempoPreparacion;
+    }
+
+    @Override
+    public String toString() {
+        return "Artículo: " +
+                "código = " + codigo + '\'' +
+                ", descripción = " + descripcion + '\'' +
+                ", PVP = " + pvp +
+                ", gastos de envío = " + gastosEnvio +
+                ", tiempo de preparacion (en minutos) = " + tiempoPreparacion + '\n';
+    }
 }
