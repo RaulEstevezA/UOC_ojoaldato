07f33ec658b004474e47142abfe82ac69f8c24c4
MarcosLlimera <mllimera@uoc.edu>
2025-10-16 13:00:25 +0200

    Update ClienteControlador
    He seguido escribiendo la clase ClienteControlador, todavía no esta termianda.

--- OjoAlDato/Source Packages/ojoaldato.controlador/ClienteControlador.java
+++ OjoAlDato/Source Packages/ojoaldato.controlador/ClienteControlador.java
@@ -2,19 +2,36 @@
 
 // Clase NO terminada
 
-package ojoaldato.controlador;
+package ojoaldato.controlador; //Indica que esta clase pertenece al paquete (carpeta) controlador, dentro de nuestra estructura MVC.
 
+// Importo Cliente (de nuestro modelo) y las clases List y ArrayList para manejar colecciones dinámicas de clientes.
 import ojoaldato.modelo.Cliente;
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class ClienteControlador {
-    public List<Cliente> clientes = new ArrayList<>();
+    public List<Cliente> clientes = new ArrayList<>(); // Aquí creo una lista donde guardaremos todos los clientes que maneja el programa.
 
-    public void agregarCliente(Cliente cliente) {
-        clientes.add(cliente);
+    public Cliente buscarPorNif(String nif) {  // He configurado este metodo para poder buscar si ya existe el CLiente o no. Si existe devolvera el Cliente y si no Null.
+        for (Cliente c : clientes) {
+            if (c.getNif().equalsIgnoreCase(nif)) {
+                return c;
+            }
+        }
+        return null;
     }
+
+    public boolean agregarCliente(Cliente cliente) {  // He puesto en boolean para así después en la Vista poder configurar más fácil.
+        if (buscarPorNif(cliente.getNif()) != null) {
+            clientes.add(cliente);
+            return true;
+        } else  {
+            return false;
+        }
+    }
+
+    // ME HE QUEDADO POR AQUÍ
 
     public void eliminarCliente(Cliente cliente) {
         clientes.remove(cliente);  // Sería interesante añadir algún tipo de busqueda y coincidencia, para asegurarse de que existe.
