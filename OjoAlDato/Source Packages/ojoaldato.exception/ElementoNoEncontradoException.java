1c86d502910769ae0b6bd100464a6882bd4cde02
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2025-10-26 15:46:01 +0100

    Añadida nueva excepcion

--- OjoAlDato/Source Packages/ojoaldato.exception/ElementoNoEncontradoException.java
+++ OjoAlDato/Source Packages/ojoaldato.exception/ElementoNoEncontradoException.java
@@ -14,19 +14,3 @@ public class ElementoNoEncontradoException extends RuntimeException {
         super(message);
     }
 }
-
-
-/**
- * Excepción personalizada que se lanza cuando se intenta añadir un elemento
- * que ya existe en una colección.
- */
-public class ElementoDuplicadoException extends RuntimeException {
-    /**
-     * Crea una nueva excepción que indica que un elemento ya está registrado.
-     *
-     * @param message Descripción del error o información adicional.
-     */
-    public ElementoDuplicadoException(String message) {
-        super(message);
-    }
-}
