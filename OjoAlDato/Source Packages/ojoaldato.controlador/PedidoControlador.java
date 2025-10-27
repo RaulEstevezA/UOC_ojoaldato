df157287b210fb38e8643236fe09bea509a6447a
MarcosLlimera <mllimera@uoc.edu>
2025-10-27 16:20:35 +0100

    Añadido metodo de crearPedido en la clase PedidoControlador de Controlador

--- OjoAlDato/Source Packages/ojoaldato.controlador/PedidoControlador.java
+++ OjoAlDato/Source Packages/ojoaldato.controlador/PedidoControlador.java
@@ -3,6 +3,7 @@ package ojoaldato.controlador;
 import ojoaldato.modelo.Pedido;
 import java.util.List;
 import ojoaldato.modelo.Datos;
+import ojoaldato.modelo.Cliente;
 
 /**
  * Clase PedidoControlador que actúa como intermediaria entre la vista
@@ -23,6 +24,15 @@ public class PedidoControlador {
      * @param pedido  Pedido a registrar
      * @return true si se crea correctamente, false si el cliente no existe
      */
+
+    public boolean crearPedido(Cliente cliente, Pedido pedido) {
+        if (datos.buscarCliente(cliente.getEmail()) != null) {  /* Aquí buscamos el clienete por su email para evitar crear pedidos para clientes que no existen */
+            datos.crearPedido(cliente, pedido);
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 
 
