49aa050bb873d75f98cec5f8348eaa000d34ca0b
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2025-10-27 13:55:21 +0100

    Cambios en la estructura

--- OjoAlDato/Source Packages/ojoaldatovista/Controller.java
+++ OjoAlDato/Source Packages/ojoaldatovista/Controller.java
@@ -1,4 +1,4 @@
-package ojoaldato.vista;
+package ojoaldatovista;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
--- OjoAlDato/Source Packages/ojoaldatovista/Launcher.java
+++ OjoAlDato/Source Packages/ojoaldatovista/Launcher.java
@@ -1,4 +1,4 @@
-package ojoaldato.vista;
+package ojoaldatovista;
 
 import javafx.application.Application;
 
