d45aff733ab05a3542dc471faffce728aa0534b5
Andrea Ros <androsrivas@outlook.com>
2025-10-27 21:54:49 +0100

    crear excepción abstracta que sirva como superclase para las excepciones personalizadas.

--- OjoAlDato/Source Packages/ojoaldato.exception/BaseException.java
+++ OjoAlDato/Source Packages/ojoaldato.exception/BaseException.java
@@ -4,4 +4,6 @@ public abstract class BaseException extends RuntimeException {
     public BaseException(String message) {
         super(message);
     }
+
+    public BaseException(String message, Throwable cause) { super(message, cause); }
 }
