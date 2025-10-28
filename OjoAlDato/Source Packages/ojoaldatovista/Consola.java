e4d864f49c09e8759d44c8fa9a0b36af42ac275a
helenavivas <helenavivas@uoc.edu>
2025-10-28 15:03:04 +0100

    Implementadas funciones de los sub-menús de Artículos y Clientes

--- OjoAlDato/Source Packages/ojoaldatovista/Consola.java
+++ OjoAlDato/Source Packages/ojoaldatovista/Consola.java
@@ -1,5 +1,14 @@
 package ojoaldatovista;
+import java.math.BigDecimal;
 import java.util.Scanner;
+import java.util.List;
+
+import ojoaldato.controlador.ArticuloControlador;
+import ojoaldato.modelo.Articulo;
+import ojoaldato.controlador.ClienteControlador;
+import ojoaldato.modelo.Cliente;
+import ojoaldato.modelo.ClienteEstandar;
+import ojoaldato.modelo.ClientePremium;
 
 // Clase para mostrar el menú principal del programa por consola
 
@@ -36,6 +45,7 @@ public class Consola {
 
     // SUBMENÚ ARTÍCULOS
     private static void menuArticulos(Scanner entrada) {
+        ArticuloControlador articulo = new ArticuloControlador();
         int opcion;
         do {
             System.out.println("\n---GESTIÓN DE ARTÍCULOS---");
@@ -45,10 +55,24 @@ public class Consola {
             System.out.println("Por favor, seleccione una opción:");
             opcion = Integer.parseInt(entrada.nextLine());
 
-            switch (opcion) {  // Pendiente de implementar
+            switch (opcion) {
                 case 1 -> {
+                    System.out.println("Código del artículo: ");
+                    String codigo = entrada.nextLine();
+                    System.out.println("Descripción del artículo: ");
+                    String descripcion = entrada.nextLine();
+                    System.out.println("PVP: ");
+                    BigDecimal pvp = new BigDecimal(entrada.nextLine());
+                    System.out.println("Gastos de envío: ");
+                    BigDecimal gastosEnvio = new BigDecimal(entrada.nextLine());
+                    System.out.println("Tiempo de preparación: ");
+                    int tiempoPreparacion = Integer.parseInt(entrada.nextLine());
+
+                    Articulo a = new Articulo(codigo, descripcion, pvp, gastosEnvio, tiempoPreparacion);
+                    System.out.println(articulo.addArticulo(a));
                 }
                 case 2 -> {
+                    List<Articulo> listarArticulos = articulo.listarArticulos();
                 }
                 case 0 -> System.out.println("Volviendo al menú principal.");
                 default -> System.out.println("Opción no válida");
@@ -58,6 +82,7 @@ public class Consola {
 
     // SUBMENÚ CLIENTES
     private static void menuClientes(Scanner entrada) {
+        ClienteControlador cliente = new ClienteControlador();
         int opcion;
         do {
             System.out.println("\n---GESTIÓN DE CLIENTES---");
@@ -69,14 +94,38 @@ public class Consola {
             System.out.println("Por favor, seleccione una opción:");
             opcion = Integer.parseInt(entrada.nextLine());
 
-            switch (opcion) { // Pendiente de implementar
+            switch (opcion) {
                 case 1 -> {
+                    System.out.println("Nombre y Apellidos: ");
+                    String nombre = entrada.nextLine();
+                    System.out.println("Domicilio: ");
+                    String domicilio = entrada.nextLine();
+                    System.out.println("NIF: ");
+                    String nif = entrada.nextLine();
+                    System.out.println("Email: ");
+                    String email = entrada.nextLine();
+                    Cliente c = new ClienteEstandar(nombre, domicilio, nif, email); //Constructor ClienteEstandar ?
+                    System.out.println(cliente.addCliente(c));
                 }
                 case 2 -> {
+                    System.out.println("Nombre y Apellidos: ");
+                    String nombre = entrada.nextLine();
+                    System.out.println("Domicilio: ");
+                    String domicilio = entrada.nextLine();
+                    System.out.println("NIF: ");
+                    String nif = entrada.nextLine();
+                    System.out.println("Email: ");
+                    String email = entrada.nextLine();
+                    System.out.println("Cuota: ");
+                    BigDecimal cuota = new BigDecimal(entrada.nextLine());
+                    Cliente c = new ClientePremium(nombre, domicilio, nif, email, cuota, null);
+                    System.out.println(cliente.addCliente(c));
                 }
                 case 3 -> {
+                    List<Cliente> listarClientesEstandar = cliente.listarClientesEstandar();
                 }
                 case 4 -> {
+                    List<Cliente> listarClientesPremium = cliente.listarClientesPremium();
                 }
                 case 0 -> System.out.println("Volviendo al menú principal.");
                 default -> System.out.println("Opción no válida");
