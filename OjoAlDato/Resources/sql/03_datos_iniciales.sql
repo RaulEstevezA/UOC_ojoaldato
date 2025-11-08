687baa2e417c1ac424808430b203b02a4bf517f6
Andrea Ros <androsrivas@outlook.com>
2025-11-08 12:32:40 +0100

    renombrar script datos iniciales

--- OjoAlDato/Resources/sql/03_datos_iniciales.sql
+++ OjoAlDato/Resources/sql/03_datos_iniciales.sql
@@ -1,6 +1,6 @@
 INSERT INTO clientes (nombre, domicilio, nif, email, tipo, descuento_envio) VALUES
-('Cliente Estándar', 'Calle Falsa 123', '12345678A', 'cliente1@ejemplo.com', 'ESTANDAR', 0),
-('Cliente Premium', 'Avenida Real 456', '87654321B', 'cliente2@ejemplo.com', 'PREMIUM', 20);
+('Cliente Estándar', 'Calle Falsa 123', '12345678A', 'cliente1@ejemplo.com', 'ESTANDAR', 0.00),
+('Cliente Premium', 'Avenida Real 456', '87654321B', 'cliente2@ejemplo.com', 'PREMIUM', 0.80);
 
 INSERT INTO articulos (codigo, descripcion, pvp, gastos_envio, tiempo_preparacion, stock) VALUES
 ('ART001', 'Artículo de prueba 1', 19.99, 4.99, 24, 100),
