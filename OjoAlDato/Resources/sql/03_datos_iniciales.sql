35284128bb36195bea19a298462a86cb0ef79d3a
Andrea Ros <androsrivas@outlook.com>
2025-11-08 12:51:04 +0100

    eliminar llamada a procedimiento almacenado crear_pedido. el equipo ha acordado que no se incluirán procedimientos almacenados porque la lógica de negocio será responsabilidad de la app con Java, y no de MySQL

--- OjoAlDato/Resources/sql/03_datos_iniciales.sql
+++ OjoAlDato/Resources/sql/03_datos_iniciales.sql
@@ -5,6 +5,3 @@ INSERT INTO clientes (nombre, domicilio, nif, email, tipo, descuento_envio) VALU
 INSERT INTO articulos (codigo, descripcion, pvp, gastos_envio, tiempo_preparacion, stock) VALUES
 ('ART001', 'Artículo de prueba 1', 19.99, 4.99, 24, 100),
 ('ART002', 'Artículo de prueba 2', 49.99, 7.99, 48, 50);
-
-CALL crear_pedido('cliente1@ejemplo.com', 'ART001', 2);
-CALL crear_pedido('cliente2@ejemplo.com', 'ART002', 1);
