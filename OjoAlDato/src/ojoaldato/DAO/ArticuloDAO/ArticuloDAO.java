4077cb9d2411ae238a8d5c5f89933826b95f5b28
Andrea Ros <androsrivas@outlook.com>
2025-11-10 11:37:10 +0100

    añadir métodos propios de gestión de Articulos: buscarArticuloPorDescripcion (deuvelve un Articulo) y buscarPorDescripcion (devuelve una lista de articulos)

--- OjoAlDato/src/ojoaldato/DAO/ArticuloDAO/ArticuloDAO.java
+++ OjoAlDato/src/ojoaldato/DAO/ArticuloDAO/ArticuloDAO.java
@@ -3,6 +3,9 @@ package ojoaldato.DAO.ArticuloDAO;
 import ojoaldato.DAO.IDAO;
 import ojoaldato.modelo.Articulo;
 
-public interface ArticuloDAO extends IDAO<Articulo, String> {
+import java.util.List;
 
+public interface ArticuloDAO extends IDAO<Articulo, String> {
+    Articulo buscarArticuloPorDescripcion(String descripcion);
+    List<Articulo> buscarPorDescripcion(String descripcion);
 }
